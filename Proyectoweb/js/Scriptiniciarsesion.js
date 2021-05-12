1e7484a20b96b8e3efe4d482894d933ef757c6a7
JesusGuirao <jesguime@gmail.com>
2021-05-13 00:31:12 +0200

    Inicio de sesion funcionando

--- Proyectoweb/js/Scriptiniciarsesion.js
+++ Proyectoweb/js/Scriptiniciarsesion.js
@@ -1,95 +1,110 @@
 /* 
 JavaScript / XML
-autor: Jesús Guirao Melero
+web o pagina: ejercicios de XML y JavaScript
+autor: Prof. Carlos Boni
+fecha: 12 mayo 2021
+resumen: lectura y carga en array bidimensional de un XML
 
+sintesis:
+la lógica se basa en el uso de tres variables de tipo sessionStorage
+usuarioLogueado : si existe, su valor es el nombre de usuario en sesion
+usuarioIntentando y claveIntentando: si existen significa que debemos
+validarlas para crear usuarioLogueado si corresponde
 */
 
 // variable global de la pagina
-let registrados = [];
 
-function leerXML() {
-  // lee desde GitHub.
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      miFuncion(this, 'Desde GitHub');
+// las siguientes variables estarán como sessionStorage
+let usrIntentando = '';
+let claveIntentando = '';
+
+function controlar() {
+  // determinamos en qué estado se carga la página:
+  // 1 - sin usuario
+  // 2 - usuario intentando ingresar
+  // 3 - usuario con sesion iniciada
+
+  if (sessionStorage.getItem('usuarioLogueado')) {
+    // estado 3 de nuestro diagrama de estados - con usuario
+    // estamos cargando la página teniendo un usuario logueado previamente
+    // y con la sesión activa pues no se ha desconectado aún
+    // ocultamos formulario de login y mostramos desconectar
+    window.location.href = '/administrador.html';
+  } else {
+    if (sessionStorage.getItem('usuarioIntentando')) {
+      // estado 2 de nuestro diagrama de estados - transición
+      // estamos recargando luego de que haya un intento de login
+      // debemos validar si el usuario existe
+      validarXML();
+      // tardo un poco en recargar para dar tiempo a AJAX?
+      for (let timer = 1; timer < 1000000; timer++);
     }
-  };
-  xhr.open(
-    'GET',
-    'https://https://jesusguirao.github.io/publico/Proyectoweb/registrados.xml',
-    true
-  );
-  xhr.send();
+  }
 }
 
-function leerXML0() {
-  // lee desde GitHub.
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      miFuncion(this, 'Desde GitHub con atributos');
-    }
-  };
-  xhr.open(
-    'GET',
-    'https://https://jesusguirao.github.io/publico/Proyectoweb/registrados2.xml',
-    true
-  );
-  xhr.send();
+function intentar() {
+  if (typeof Storage !== 'undefined') {
+    // Almacena un valor usando el método setItem del objeto localStorage
+    var x = document.forms['miFormulario']['formUsuario'].value;
+    var y = document.forms['miFormulario']['formClave'].value;
+    sessionStorage.setItem('usuarioIntentando', x);
+    sessionStorage.setItem('claveIntentando', y);
+
+    // ya tengo en memoria webStorage lo que puso en el formulario
+    // al recargarse la página podré recordar esta información
+  } else {
+    document.getElementById('mensaje').innerHTML =
+      'Este navegador no soporta web storage...';
+  }
 }
 
-function leerXML1() {
-  // lee desde Axartec.
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      miFuncion(this, 'Desde Axartec');
-    }
-  };
-  xhr.open('GET', 'https://appslab.axartec.com/pruebas/registrados.xml', true);
-  xhr.send();
-}
-
-function leerXML2() {
+function validarXML() {
   // lee desde aquí.
+  usuarioIntentando = sessionStorage.getItem('usuarioIntentando');
+  claveIntentando = sessionStorage.getItem('claveIntentando');
+  sessionStorage.removeItem('usuarioIntentando');
+  sessionStorage.removeItem('claveIntentando');
+
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      miFuncion(this, 'Desde aquí');
+      miFuncion(this);
     }
   };
-  xhr.open('GET', 'registrados.xml', true);
+  xhr.open(
+    'GET',
+    'https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml',
+    true
+  );
   xhr.send();
 }
 
-function miFuncion(xml, fuente) {
+function miFuncion(xml) {
   var i;
   var usrNom;
   var usrPsw;
   var usuario = [];
   var xmlDoc = xml.responseXML;
-  var tabla = fuente + '<br/>';
   var x = xmlDoc.getElementsByTagName('usuario');
+  sessionStorage.removeItem('usuarioLogueado');
 
-  tabla += '<table><tr><th>Empleado</th><th>Clave</th></tr>';
   for (i = 0; i < x.length; i++) {
     // leo las etiquetas que me interesan del objeto
     usrNom = x[i].getElementsByTagName('nombre')[0].childNodes[0].nodeValue;
     usrPsw = x[i].getElementsByTagName('clave')[0].childNodes[0].nodeValue;
     // actualizo la tabla de visualización
-    tabla += '<tr><td>' + usrNom + '</td><td>' + usrPsw + '</td></tr>';
-    // actualizo el array bidimensional con los usuarios registrados
-    usuario = [usrNom, usrPsw];
-    registrados.push(usuario);
+    if (usrNom == usuarioIntentando && usrPsw == claveIntentando) {
+      // destaca el usuario que coincide con lo que buscamos
+      sessionStorage.setItem('usuarioLogueado', usuarioIntentando);
+    }
   }
-  tabla += '</table>';
-  document.getElementById('contenidoXML').innerHTML = tabla;
 
-  // muestro en consola el array de usuarios registrados
-  registrados.forEach((usuario) => {
-    usuario.forEach((datos) => {
-      console.log(datos);
-    });
-  });
+  controlar();
+}
+
+function desconectar() {
+  sessionStorage.removeItem('usuarioLogueado');
+  sessionStorage.removeItem('usuarioIntentando');
+  sessionStorage.removeItem('claveIntentando');
+  location.reload();
 }
