270c013f9649c194817b984f0a5b14d6ed24dfeb
JesusGuirao <jesguime@gmail.com>
2021-05-21 13:02:01 +0200

    Quien se ha logueado en dolly

--- Proyectoweb/js/ScriptMarbella.js
+++ Proyectoweb/js/ScriptMarbella.js
@@ -1,3 +1,11 @@
+/* BOTON Volver atrás*/
 function goBack() {
   window.history.back();
+}
+
+/* quien hay conectado */
+
+if (sessionStorage.getItem('usuarioLogueado') != null) {
+  document.getElementById('enlinea').textContent =
+    'Estás Logueado Con:' + sessionStorage.getItem('usuarioLogueado');
 }
